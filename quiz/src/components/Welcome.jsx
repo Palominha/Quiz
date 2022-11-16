742c26e46c0323fc644668f3aeea7293f348bd52
Paloma <palomatj.gomes5@gmail.com>
2022-11-16 20:10:11 -0300

    Projeto finalizado

---
+++ quiz/src/components/Welcome.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
+import "./Welcome.css";
+
+import Quiz from "../img/Questions.svg";
+
+const Welcome = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  return (
+    <div id="welcome">
+      <h2>Seja bem-vindo</h2>
+      <p>Que tal testar seus conhecimentos? </p>
+      <button onClick={() => dispatch({ type: "CHANGE_STAGE" })}>
+        Iniciar
+      </button>
+      <img src={Quiz} alt="Início do Quiz" />
+    </div>
+  );
+};
+
+export default Welcome;
